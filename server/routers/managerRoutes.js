6c9a6ae589f076843b2e0e8ef972657cc8793d86
RMIT-son <119044913+RMIT-son@users.noreply.github.com>
2024-01-09 11:08:42 +0700

    Update managerRoutes.js

--- server/routers/managerRoutes.js
+++ server/routers/managerRoutes.js
@@ -22,4 +22,16 @@ router.get('/', async (req, res) => {
 });
 
 
+router.get('/overall', async (req, res) => {
+    try {
+        let instance = axios.create({baseURL: `${req.protocol}://${req.get('host')}`});
+        let artworks = await instance("/api/art/get")
+        artworks = await artworks.data;
+        res.render("./manager/manager", { artworks: artworks });
+    } catch (error) {
+        console.error('Error fetching artworks:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 module.exports = router;
