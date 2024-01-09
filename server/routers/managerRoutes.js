01151c80eb480617f1533f0cd7642bb21043de1a
Duy3978690 <121443725+Duy3978690@users.noreply.github.com>
2024-01-09 21:59:37 +0700

    xongg

--- server/routers/managerRoutes.js
+++ server/routers/managerRoutes.js
@@ -1,37 +1,36 @@
-require('dotenv').config({ path: '../../.env' });
-const express = require('express');
-const { UnauthorizedError } = require('express-oauth2-jwt-bearer');
+require("dotenv").config({ path: "../../.env" });
+const express = require("express");
+const { UnauthorizedError } = require("express-oauth2-jwt-bearer");
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try {
-        let artworks = await fetch("http://localhost:3000/api/art/get")
-        artworks = await artworks.json()
-        if (req.oidc.isAuthenticated()) {
-            if (req.oidc.user.email === "admin@museum.com") {
-                res.render("./manager/manager", { artworks: artworks });
-            }
-            res.status(401).send('Unauthorized access');
-        } else {
-            res.status(401).send('Unauthorized access');
-        }
-    } catch (error) {
-        console.error('Error fetching artworks:', error.message);
-        res.status(500).send('Internal Server Error');
+router.get("/", async (req, res) => {
+  try {
+    if (req.oidc.isAuthenticated()) {
+      if (req.oidc.user.email === "admin@museum.com") {
+        res.render("./manager/manager", { user: req.oidc.user });
+      }
     }
+  } catch (error) {
+    console.error("Error fetching artworks:", error.message);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
-
-router.get('/overall', async (req, res) => {
-    try {
-        let instance = axios.create({baseURL: `${req.protocol}://${req.get('host')}`});
-        let artworks = await instance("/api/art/get")
-        artworks = await artworks.data;
-        res.render("./manager/manager", { artworks: artworks });
-    } catch (error) {
-        console.error('Error fetching artworks:', error.message);
-        res.status(500).send('Internal Server Error');
-    }
+router.get("/overall", async (req, res) => {
+  try {
+    let instance = axios.create({
+      baseURL: `${req.protocol}://${req.get("host")}`,
+    });
+    let artworks = await instance("/api/art/get");
+    artworks = await artworks.data;
+    res.render("./manager/manager", {
+      artworks: artworks,
+      user: req.oidc.user,
+    });
+  } catch (error) {
+    console.error("Error fetching artworks:", error.message);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
--- server/routers/overallRoutes.js
+++ server/routers/overallRoutes.js
@@ -5,7 +5,7 @@ const artModel = require("../models/artModel.js");
 router.get('/', async (req, res) => {
     try {
         const artworks = await artModel.find({});
-        res.render("./manager/overall", {artworks: artworks,});
+        res.render("./manager/overall", {artworks: artworks, user: req.oidc.user});
     } catch (e) {
         console.log(e);
     }
