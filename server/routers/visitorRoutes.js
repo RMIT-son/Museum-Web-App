98161c5d2a2dcb3b36004039ca1320f26c214a89
dathoangquoc <dathgqc@gmail.com>
2024-01-09 21:21:24 +0700

    redirect admin to panel

--- server/routers/visitorRoutes.js
+++ server/routers/visitorRoutes.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render("index");
+    if (req.oidc.isAuthenticated()) {
+        if (req.oidc.user.email === "admin@museum.com") {
+            res.redirect('manager');
+        } else {
+            res.render("index");
+        }
+    } else {
+        res.render("index");
+    }
 });
 
 module.exports = router;
