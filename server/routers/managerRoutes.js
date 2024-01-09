21c59479182e23bba3a89f43ff92c0d861e351fb
dathoangquoc <dathgqc@gmail.com>
2024-01-09 22:08:50 +0700

    fix header bug

--- server/routers/managerRoutes.js
+++ server/routers/managerRoutes.js
@@ -6,16 +6,13 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        let instance = axios.create({baseURL: `${req.protocol}://${req.get('host')}`});
+        let instance = axios.create({ baseURL: `${req.protocol}://${req.get('host')}` });
         let artworks = await instance("/api/art/get")
         artworks = await artworks.data;
         if (req.oidc.isAuthenticated()) {
             if (req.oidc.user.email === "admin@museum.com") {
                 res.render("./manager/manager2", { artworks: artworks });
             }
-            res.status(401).send('Unauthorized access');
-        } else {
-            res.status(401).send('Unauthorized access');
         }
     } catch (error) {
         console.error('Error fetching artworks:', error.message);
@@ -26,7 +23,7 @@ router.get('/', async (req, res) => {
 
 router.get('/stat', async (req, res) => {
     try {
-        let instance = axios.create({baseURL: `${req.protocol}://${req.get('host')}`});
+        let instance = axios.create({ baseURL: `${req.protocol}://${req.get('host')}` });
         let artworks = await instance("/api/art/get")
         artworks = await artworks.data;
         res.render("./manager/manager", { artworks: artworks });
@@ -38,10 +35,10 @@ router.get('/stat', async (req, res) => {
 
 router.get('/overall', async (req, res) => {
     try {
-        let instance = axios.create({baseURL: `${req.protocol}://${req.get('host')}`});
+        let instance = axios.create({ baseURL: `${req.protocol}://${req.get('host')}` });
         let artworks = await instance("/api/art/get")
         artworks = await artworks.data;
-        res.render("./manager/overall", {artworks: artworks});
+        res.render("./manager/overall", { artworks: artworks });
     } catch (error) {
         console.error('Error fetching artworks:', error.message);
         res.status(500).send('Internal Server Error');
