189830acd545fc7f27d5a0f80ebd69b9e070d835
RMIT-son <119044913+RMIT-son@users.noreply.github.com>
2024-01-09 21:35:46 +0700

    Update visitorRoutes.js

--- server/routers/visitorRoutes.js
+++ server/routers/visitorRoutes.js
@@ -2,12 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    if (req.oidc.isAuthenticated()) {
-        if (req.oidc.user.email === "admin@museum.com") {
+    if (req.oidc.isAuthenticated()) and (req.oidc.user.email === "admin@museum.com") {
             res.redirect('manager');
-        } else {
-            res.render("index");
-        }
     } else {
         res.render("index");
     }
